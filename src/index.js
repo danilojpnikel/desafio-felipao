a2688ccbdf7153bf1af7b61b302d13a2a004f430
danilojpnikel <daniilobakasu@gmail.com>
2023-12-13 23:07:32 -0300

    desafio finalizado

--- src/index.js
+++ src/index.js
@@ -6,19 +6,19 @@ let xp = 3200
 // XP e Elo personagem 
 
   if(xp <= 1000){
-    console.log(" O Herói de nome " + id + " Está no Nível de Ferro. Parabéns! ")
+    console.log(" O Herói de nome " + id + " Está no Nível de Ferro. ")
 }else if(xp > 1001 && xp <= 2000){
-    console.log(" O Herói de nome " + id + " Está no Nível de Bronze. Parabéns!  ")
+    console.log(" O Herói de nome " + id + " Está no Nível de Bronze. ")
 }else if(xp > 2001 && xp <= 5000){
-    console.log(" O Herói de nome " + id + " Está no Nível de Prata. Parabéns! ")
+    console.log(" O Herói de nome " + id + " Está no Nível de Prata. ")
 }else if(xp > 5001 && xp <= 7000){
-    console.log(" O Herói de nome " + id + " Está no Nível de Ouro. Parabéns! ")
+    console.log(" O Herói de nome " + id + " Está no Nível de Ouro. ")
 }else if(xp  > 7001 && xp <= 8000){
-    console.log(" O Herói de nome " + id + " Está no Nível de Platina. Parabéns! ")
+    console.log(" O Herói de nome " + id + " Está no Nível de Platina. ")
 }else if(xp > 8001 && xp <= 9000){
-    console.log(" O Herói de nome " + id + " Está no Nível de Ascendente. Parabéns! ")
+    console.log(" O Herói de nome " + id + " Está no Nível de Ascendente. ")
 }else if(xp > 9001 && xp <= 10000){
-    console.log(" O Herói de nome " + id + " Está no Nível de Imortal. Parabéns! ")
+    console.log(" O Herói de nome " + id + " Está no Nível de Imortal. ")
 }else if(xp >= 10000){
-    console.log(" O Herói de nome " + id + " Está no Nível de Radiante. Parabéns! ")
+    console.log(" O Herói de nome " + id + " Está no Nível de Radiante. ")
 }
