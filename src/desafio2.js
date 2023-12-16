60bb3bf2723b4a5ac46f3285919e4e67599ca314
danilojpnikel <daniilobakasu@gmail.com>
2023-12-16 01:28:29 -0300

    desafio 2 finlizado

--- src/desafio2.js
+++ src/desafio2.js
@@ -1,31 +1,29 @@
 // Rank do personagem
 
 
-function CalcNivel(vitorias, derrotas){
-    let totalVitorias = vitorias - derrotas
-    let level
-
-    if (totalVitorias < 10){
-        level = "Ferro" 
-    } else if (totalVitorias >= 11 && totalVitorias <= 20){
-        level = "Bronze"
-    } else if (totalVitorias >= 21 && totalVitorias <=30){
-        level = "Prata"
-    } else if (totalVitorias >= 31 && totalVitorias <=40){
-        level = "Ouro"
-    } else if (totalVitorias >= 41 && totalVitorias <=50){
-        level = "Diamante"
-    } else if (totalVitorias >= 51 && totalVitorias <=70){
-        level = "lendario"
-    } else if (totalVitorias >= 71 && totalVitorias <= 100){
-        level = "Imortallll"
+function calcNivel(vitorias, derrotas) {
+    let totalVitorias = vitorias - derrotas;
+    let level;
+  
+    if (totalVitorias < 10) {
+      level = "Ferro";
+    } else if (totalVitorias <= 20) {
+      level = "Bronze";
+    } else if (totalVitorias <= 30) {
+      level = "Prata";
+    } else if (totalVitorias <= 40) {
+      level = "Ouro";
+    } else if (totalVitorias <= 50) {
+      level = "Diamante";
+    } else if (totalVitorias <= 70) {
+      level = "Lendário";
+    } else if (totalVitorias <= 100) {
+      level = "Imortal";
     }
-    
-        
-    
-    return {totalVitorias, level}
-    let (totalVitorias , level) = CalcNivel (66)
-
-    console.log("O Herói tem de saldo de **{totalVitorias}** está no nível de **{level}")
-}
-
+    return { totalVitorias, level };
+  }
+  
+  const resultado = calcNivel(66, 34);
+  
+  console.log(`O Herói tem um saldo total de ${resultado.totalVitorias} vitórias e está no nível ${resultado.level}.`);
+  
